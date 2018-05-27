6a4411a90e02fd598fdc99e3e110a349ad2db0b1
Mou-Chun Wang <jimmyexx@gmail.com>
2018-05-28 00:08:44 +0800

    1208: Use Switch case"

--- index.js
+++ index.js
@@ -9,9 +9,62 @@ exports.handler = function (req, res) {
 
     const promises = req.events.map(event => {
 
-        var msg = event.message.text.toUpperCase();
-
         console.log('I am here');
+
+        switch(event.message.type)
+        {
+            case 'text':
+                var msg = event.message.text.toUpperCase();
+                break;
+
+            case 'image':
+                // var msg = 'You Send an Image';
+                // gotUserMessage = false;
+                var img_id = event.message.id;
+                content_url = "https://api.line.me/v2/bot/message/" + img_id + "/content";
+
+                //Make get Request
+                var requestSettings  = {
+                    url: content_url,
+                    headers: {
+                        "Content-type": "application/json; charset=UTF-8",
+                        "Authorization": " Bearer " + process.env.CHANNEL_ACCESS_TOKEN
+                    },
+                    method: 'GET',
+                    encoding: null
+                };
+
+                request(requestSettings, function(error, response, body) {
+                    // Use body as a binary Buffer
+                    //imageName = 'WeddingHelper/L30/photos/' + img_id + '.jpg';
+                    imageName = img_id + '.jpg';
+
+                    //Here Upload to S3
+                    var s3Bucket = new AWS.S3({params:{Bucket:S3Imagebucket} });
+                    var data = {
+                        Key: imageName,
+                        Body: body,
+                        ContentType: "image/jpeg"
+                    };
+
+                    s3Bucket.upload(data, function(err, data){
+                        if (err)
+                        { console.log('Error uploading data: ', data);}
+                        else
+                        {
+                            console.log('Successfully uploaded the image! Yo');
+                        }
+                    });
+                });//end of request
+
+                msg = 'PhotoUploaded!';
+                break;
+
+            default:
+                var msg = 'Hmmmm';
+                break;
+        }//end of Switch case
+
 
         var replyMessages = [
             {
@@ -19,51 +72,6 @@ exports.handler = function (req, res) {
                 "text": msg
             }
         ];
-
-        //User pass-in images
-        if(event.message.type == 'image')
-        {
-            gotUserMessage = false;
-            var img_id = event.message.id;
-            content_url = "https://api.line.me/v2/bot/message/" + img_id + "/content";
-
-            //Make get Request
-            var requestSettings  = {
-                url: content_url,
-                headers: {
-                    "Content-type": "application/json; charset=UTF-8",
-                    "Authorization": " Bearer " + process.env.CHANNEL_ACCESS_TOKEN
-                },
-                method: 'GET',
-                encoding: null
-            };
-
-            request(requestSettings, function(error, response, body) {
-                // Use body as a binary Buffer
-                //imageName = 'WeddingHelper/L30/photos/' + img_id + '.jpg';
-
-                imageName = img_id + '.jpg';
-
-                //Here Upload to S3
-                var s3Bucket = new AWS.S3({params:{Bucket:S3Imagebucket} });
-                var data = {
-                    Key: imageName,
-                    Body: body,
-                    ContentType: "image/jpeg"
-                };
-
-                s3Bucket.upload(data, function(err, data){
-                    if (err)
-                    { console.log('Error uploading data: ', data);}
-                    else
-                    {
-                        console.log('Successfully uploaded the image! Yo');
-                    }
-                });
-            });//end of request
-
-            msg = 'PhotoUploaded!';
-        }//endif;
 
         needToReply = true;
 
