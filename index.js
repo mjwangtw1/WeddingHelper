70fa483b842fd928ddb3d73014c99cc04657a447
Mou-Chun Wang <jimmyexx@gmail.com>
2018-05-27 22:41:56 +0800

    Trying with Simple ones

--- index.js
+++ index.js
@@ -3,11 +3,18 @@ exports.handler = (event, context, callback) => {
 
     const ChannelAccessToken = process.env.CHANNEL_ACCESS_TOKEN;
 
+    console.log('Yolo~Debugging');
+    console.log(event);
+
     var reply_token = event.replyToken; //Need to get this Token to pass back.
     var messages = [{
         "type":"text",
-        "text": event.message.text
+       // "text": event.message.text
+        "text":"我李奧94崩"
     }];
+
+
+
 
     callback(null, messages);
 };
